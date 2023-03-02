1ab89bc27f1194184b346e7c77304123f520390b
Ethan Vaughn <ethancvaughn@gmail.com>
2023-03-01 18:51:31 -0700

    Add project files.

---
+++ Bookstore/Models/EFBookstoreRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class EFBookstoreRepo : IBookstoreRepo
+    {
+
+        private BookstoreContext context { get; set; }
+
+        public EFBookstoreRepo (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Books> Books => context.Books;
+    }
+}
